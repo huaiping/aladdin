7072284e45a89b435f437a95457a179f79af7034
huaiping <huaiping@live.cn>
2015-07-20 11:34:16 +0800

    initial import

--- application/views/plaza_article.php
+++ application/views/plaza_article.php
@@ -40,7 +40,7 @@
                     </div>
                 </div>
                 <div class="form-group">
-                    <label for="content" class="col-md-1 control-label">描述</label>
+                    <label for="content" class="col-md-1 control-label">正文</label>
                     <div class="col-xs-9 col-sm-11 col-md-11"><textarea id="content" name="content" class="form-control col-md-12" rows="20" style="visibility:hidden;"></textarea></div>
                 </div>
                 <div class="form-group">
