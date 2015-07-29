e43783f8bab9b15e5413128c644d7b10cee52462
huaiping <huaiping@live.cn>
2015-07-29 14:06:39 +0800

    initial import

--- application/models/Photo_model.php
+++ application/models/Photo_model.php
@@ -6,28 +6,28 @@ class Photo_model extends CI_Model {
         $this->load->database();
     }
 
-    public function get_category($slug = FALSE)
+    public function get_category($id = FALSE)
     {
-        if ($slug === FALSE)
+        if ($id === FALSE)
         {
             $this->db->select('id, category');
             $this->db->group_by('category');
             $query = $this->db->get('swan_photo');
             return $query->result_array();
         }
-        $query = $this->db->get_where('swan_photo', array('id' => $slug));
+        $query = $this->db->get_where('swan_photo', array('id' => $id));
         return $query->row_array();
     }
 
-    public function get_subcategory($slug = FALSE)
+    public function get_subcategory($id = FALSE)
     {
-        if ($slug === FALSE)
+        if ($id === FALSE)
         {
             $this->db->group_by('subcategory');
             $query = $this->db->get('swan_photo');
             return $query->result_array();
         }
-        $query = $this->db->get_where('swan_photo', array('id' => $slug));
+        $query = $this->db->get_where('swan_photo', array('id' => $id));
         return $query->row_array();
     }
 
@@ -54,7 +54,9 @@ class Photo_model extends CI_Model {
             $query = $this->db->get('swan_photo');
             return $query->result_array();
         }
-        $query = $this->db->query("SELECT * FROM swan_photo limit $offset, $num");
+        //$query = $this->db->query("SELECT * FROM swan_photo limit $offset, $num");
+        $this->db->limit($num, $offset);
+        $query = $this->db->get('swan_photo');
         return $query->result_array();
     }
 }
