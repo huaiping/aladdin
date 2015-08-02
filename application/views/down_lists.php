cc5c33999f595f99ce408dd223c3bf6013e9aae9
huaiping <huaiping@live.cn>
2015-08-02 10:32:44 +0800

    initial import

--- application/views/down_lists.php
+++ application/views/down_lists.php
@@ -15,7 +15,7 @@
             <div class="widget-box no-border">
                 <div class="widget-box_title bs-callout bs-callout-danger"><h4>热门资源</h4></div>
                     <ul class="widget-links list-unstyled">
-                        <?php foreach ($down_hot as $hot):?>
+                        <?php foreach ($down_hot as $hot): ?>
                             <li class="widget-links_item"><a href="<?php echo site_url('download/view/'.$hot['id']); ?>"><?php echo $hot['title']; ?></a></li>
                         <?php endforeach; ?>
                     </ul>
