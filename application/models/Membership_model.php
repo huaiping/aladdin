8ef4d39832ae000761263bf12af8185ed24ce9ff
huaiping <huaiping@live.cn>
2015-08-03 14:58:26 +0800

    initial import

--- application/models/Membership_model.php
+++ application/models/Membership_model.php
@@ -28,15 +28,18 @@ class Membership_model extends CI_Model {
             //$query = $this->db->get('swan_news');
             //return $query->result_array();
         }
-        $data = array(
-            'title' => $this->input->post('title'),
-            'category' => $this->input->post('category'),
-            'content' => $this->input->post('content'),
-            'author' => $this->input->post('username'),
-            'pubtime' => $now,
-            'status' => 'draft'
-        );
-        $this->db->insert('swan_news', $data);
+        else
+        {
+            $data = array(
+                'title' => $this->input->post('title'),
+                'category' => $this->input->post('category'),
+                'content' => $this->input->post('content'),
+                'author' => $this->input->post('username'),
+                'pubtime' => $now,
+                'status' => 'draft'
+            );
+            $this->db->insert('swan_news', $data);
+        }
     }
 
     public function filesPost($title = FALSE, $category = FALSE, $url = FALSE, $content = FALSE, $license = FALSE, $username = FALSE)
@@ -47,17 +50,20 @@ class Membership_model extends CI_Model {
             //$query = $this->db->get('swan_news');
             //return $query->result_array();
         }
-        $data = array(
-            'title' => $this->input->post('title'),
-            'category' => $this->input->post('subject').$this->input->post('category'),
-            'url' => $this->upload->data('file_name'),
-            'description' => $this->input->post('content'),
-            'author' => $this->input->post('username'),
-            'pubtime' => $now,
-            'license' => '共享版',
-            'status' => 'draft'
-        );
-        $this->db->insert('swan_download', $data);
+        else
+        {
+            $data = array(
+                'title' => $this->input->post('title'),
+                'category' => $this->input->post('subject').$this->input->post('category'),
+                'url' => $this->upload->data('file_name'),
+                'description' => $this->input->post('content'),
+                'author' => $this->input->post('username'),
+                'pubtime' => $now,
+                'license' => '共享版',
+                'status' => 'draft'
+            );
+            $this->db->insert('swan_download', $data);
+        }
     }
 
     public function messagePost($title = FALSE, $content = FALSE, $username = FALSE)
@@ -68,14 +74,17 @@ class Membership_model extends CI_Model {
             //$query = $this->db->get('swan_news');
             //return $query->result_array();
         }
-        $data = array(
-            'title' => $this->input->post('title'),
-            'content' => $this->input->post('content'),
-            'author' => $this->input->post('username'),
-            'pubtime' => $now,
-            'status' => 'draft'
-        );
-        $this->db->insert('swan_guestbook', $data);
+        else
+        {
+            $data = array(
+                'title' => $this->input->post('title'),
+                'content' => $this->input->post('content'),
+                'author' => $this->input->post('username'),
+                'pubtime' => $now,
+                'status' => 'draft'
+            );
+            $this->db->insert('swan_guestbook', $data);
+        }
     }
 
 }
