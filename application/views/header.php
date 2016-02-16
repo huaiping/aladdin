d41c0fd297db6acb34e7bf6556678cc9e3cf303e
huaiping <huaiping@live.cn>
2016-02-16 12:05:54 +0800

    update highcharts to 4.2.3

--- application/views/header.php
+++ application/views/header.php
@@ -65,7 +65,7 @@ blockquote p {font-size:16px;}
 <script src="//cdn.bootcss.com/bootstrap-select/1.9.4/js/bootstrap-select.min.js"></script>
 <script src="//cdn.bootcss.com/jquery.qrcode/1.0/jquery.qrcode.min.js"></script>
 <script src="//cdn.bootcss.com/jquery.form/3.51/jquery.form.min.js"></script>
-<script src="//cdn.bootcss.com/highcharts/4.2.1/highcharts.js"></script>
+<script src="//cdn.bootcss.com/highcharts/4.2.3/highcharts.js"></script>
 <script src="<?php echo base_url('webroot/jquery.grid-a-licious.min.js'); ?>"></script>
 </head>
 <body>
