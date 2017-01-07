335b3c8a403005811ed3671fa1430193c6113b20
huaiping <huaiping@live.cn>
2017-01-07 12:34:12 +0800

    updated

--- application/views/footer.php
+++ application/views/footer.php
@@ -4,7 +4,7 @@
     <div class="container bg-primary">
         <div class="row">
             <div class="col-xs-12 col-sm-10 col-md-10">
-                <div style="height:30px; line-height:30px; margin-top:10px;">Copyright &copy; 2015-2016 ♂古月Sir@阿拉丁CMS</div>
+                <div style="height:30px; line-height:30px; margin-top:10px;">Copyright &copy; 2015-2017 ♂古月Sir@阿拉丁CMS</div>
                 <div style="height:30px; line-height:30px; margin-bottom:10px;">鄂ICP备15002830号 Powered by 阿拉丁CMS</div>
             </div>
             <div class="col-sm-2 col-md-2 text-right hidden-xs" style="margin-top:24px;">
