6b8e454818e6a0f249e3da58565db2b36ddd9783
huaiping <huaiping@live.cn>
2017-07-22 20:51:28 +0800

    updated

--- application/views/score.php
+++ application/views/score.php
@@ -10,7 +10,7 @@
     </div>
 
     <div class="score row">
-        <div class="col-xs-12 col-md-9">
+        <div class="col-xs-12 col-md-8">
             <!--网站通知 begin-->
             <div class="alert alert-warning alert-dismissible" role="alert">
                 <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
@@ -76,7 +76,7 @@
             </div>
         </div>
 
-        <div class="col-xs-11 col-md-3">
+        <div class="col-xs-11 col-md-4">
             <!--成绩查询 begin-->
             <div class="searchBox row">
                 <h2 class="text-center" style="margin-bottom:20px;">成绩查询</h2>
@@ -85,7 +85,7 @@
                      echo form_open('score/view', $attributes);
                 ?>
                     <div class="form-group">
-                        <div class="col-xs-11 col-sm-10 col-md-10 col-xs-offset-1 col-md-offset-1">
+                        <div class="col-xs-11 col-sm-10 col-md-9 col-xs-offset-1 col-md-offset-1">
                             <select class="selectpicker form-control" id="category" name="category">
                                 <option value="1">2015年春高一期末考试</option>
                                 <option value="1|2">2015年春高二期末考试</option>
@@ -93,13 +93,13 @@
                         </div>
                     </div>
                     <div class="form-group">
-                        <div class="col-xs-11 col-sm-10 col-md-10 col-xs-offset-1 col-md-offset-1">
+                        <div class="col-xs-11 col-sm-10 col-md-9 col-xs-offset-1 col-md-offset-1">
                             <input type="text" class="form-control" id="name" name="name" maxlength="4" placeholder="姓名" value="" required="required">
                         </div>
                     </div>
                     <div class="form-group">
-                        <div class="col-xs-11 col-sm-10 col-md-10 col-xs-offset-1 col-md-offset-1">
-                            <input type="text" class="form-control" id="number" name="number" maxlength="18" placeholder="身份证号 学号 班级号" value="" required="required">
+                        <div class="col-xs-11 col-sm-10 col-md-9 col-xs-offset-1 col-md-offset-1">
+                            <input type="text" class="form-control" id="number" name="number" maxlength="18" placeholder="学号/班级号/身份证号" value="" required="required">
                         </div>
                     </div>
                     <div class="form-group">
