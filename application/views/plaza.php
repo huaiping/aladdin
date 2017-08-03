385af0264f10be93dc7ac31495c05621d65528d1
huaiping <huaiping@live.cn>
2017-08-03 09:25:40 +0800

    plaza and score layout fixed

--- application/views/plaza.php
+++ application/views/plaza.php
@@ -12,15 +12,11 @@
         </div>
 
         <div class="col-md-9">
-        <?php
-            echo $this->input->user_agent();
-        ?>
-
            <div class="col-md-6">
                 <!--投稿 begin-->
                 <div class="widget-box no-border">
-                    <div class="widget-box_title bs-callout bs-callout-info"><h4>我的投稿</h4></div>
-                    <ul class="widget-links list-unstyled">
+                    <div class="widget-box_title bs-callout bs-callout-info"><h4>我的投稿<span class="pull-right glyphicon glyphicon-menu-hamburger" style="padding-right:10px;"></span></h4></div>
+                    <ul class="widget-links list-unstyled" style="min-height:160px;">
                     <?php foreach ($articles as $myarticle): ?>
                         <li class="widget-links_item"><a href="#"><?php echo $myarticle['title']; ?></a></li>
                     <?php endforeach ?>
@@ -32,8 +28,8 @@
             <div class="col-md-6">
                 <!--相册 begin-->
                 <div class="widget-box no-border">
-                    <div class="widget-box_title bs-callout bs-callout-info"><h4>我的相册</h4></div>
-                    <ul class="widget-links list-unstyled">
+                    <div class="widget-box_title bs-callout bs-callout-info"><h4>我的相册<span class="pull-right glyphicon glyphicon-menu-hamburger" style="padding-right:10px;"></span></h4></div>
+                    <ul class="widget-links list-unstyled" style="min-height:160px;">
                     <?php foreach ($albums as $myalbum): ?>
                         <li class="widget-links_item"><a href="#"><?php echo $myalbum['id']; ?></a></li>
                     <?php endforeach ?>
@@ -45,8 +41,8 @@
             <div class="col-md-6">
                 <!--文件 begin-->
                 <div class="widget-box no-border">
-                    <div class="widget-box_title bs-callout bs-callout-info"><h4>我的文件</h4></div>
-                    <ul class="widget-links list-unstyled">
+                    <div class="widget-box_title bs-callout bs-callout-info"><h4>我的文件<span class="pull-right glyphicon glyphicon-menu-hamburger" style="padding-right:10px;"></span></h4></div>
+                    <ul class="widget-links list-unstyled" style="min-height:160px;">
                     <?php foreach ($files as $myfile): ?>
                         <li class="widget-links_item"><a href="#"><?php echo $myfile['title']; ?></a></li>
                     <?php endforeach ?>
@@ -58,8 +54,8 @@
             <div class="col-md-6">
                 <!--留言 begin-->
                 <div class="widget-box no-border">
-                    <div class="widget-box_title bs-callout bs-callout-info"><h4>我的留言</h4></div>
-                    <ul class="widget-links list-unstyled">
+                    <div class="widget-box_title bs-callout bs-callout-info"><h4>我的留言<span class="pull-right glyphicon glyphicon-menu-hamburger" style="padding-right:10px;"></span></h4></div>
+                    <ul class="widget-links list-unstyled" style="min-height:160px;">
                     <?php foreach ($messages as $mymessage): ?>
                         <li class="widget-links_item"><a href="#"><?php echo $mymessage['title']; ?></a></li>
                     <?php endforeach ?>
@@ -71,8 +67,8 @@
             <div class="col-md-6">
                 <!--视频 begin-->
                 <div class="widget-box no-border">
-                    <div class="widget-box_title bs-callout bs-callout-info"><h4>我的视频</h4></div>
-                    <ul class="widget-links list-unstyled">
+                    <div class="widget-box_title bs-callout bs-callout-info"><h4>我的视频<span class="pull-right glyphicon glyphicon-menu-hamburger" style="padding-right:10px;"></span></h4></div>
+                    <ul class="widget-links list-unstyled" style="min-height:160px;">
                     <?php foreach ($videos as $myvideo): ?>
                         <li class="widget-links_item"><a href="#"><?php echo $myvideo['title']; ?></a></li>
                     <?php endforeach ?>
@@ -84,8 +80,8 @@
             <div class="col-md-6">
                 <!--链接 begin-->
                 <div class="widget-box no-border">
-                    <div class="widget-box_title bs-callout bs-callout-info"><h4>我的链接</h4></div>
-                    <ul class="widget-links list-unstyled">
+                    <div class="widget-box_title bs-callout bs-callout-info"><h4>我的链接<span class="pull-right glyphicon glyphicon-menu-hamburger" style="padding-right:10px;"></span></h4></div>
+                    <ul class="widget-links list-unstyled" style="min-height:160px;">
                     <?php foreach ($links as $mylink): ?>
                         <li class="widget-links_item"><a href="#"><?php echo $mylink['title']; ?></a></li>
                     <?php endforeach ?>
@@ -93,7 +89,6 @@
                 </div>
                 <!--链接 end-->
             </div>
-
         </div>
     </div>
 </section>
--- application/views/score.php
+++ application/views/score.php
@@ -25,7 +25,7 @@
                     </div>
                     <div class="media-body">
                         <h4 class="media-heading" style="margin-bottom:10px;">使用指南</h4>
-                        <p>选择查询项目，例如：2015年秋高一期中考试；姓名栏输入姓名，该姓名需与学籍姓名一致；学号栏输入对应的学号、班级号或身份证号。</p>
+                        <p style="min-height:80px;">选择查询项目，例如：2015年秋高一期中考试；姓名栏输入姓名，该姓名需与学籍姓名一致；学号栏输入对应的学号、班级号或身份证号。</p>
                     </div>
                 </div>
                 <!--使用指南 end-->
@@ -39,7 +39,7 @@
                     </div>
                     <div class="media-body">
                         <h4 class="media-heading" style="margin-bottom:10px;">温馨提示</h4>
-                        <p>建议使用360浏览器、谷歌浏览器、猎豹浏览器、QQ浏览器、UC浏览器、搜狗浏览器、百度浏览器、Internet Explorer的最新版，以获得最佳体验。</p>
+                        <p style="min-height:80px;">建议使用360浏览器、谷歌浏览器、UC浏览器、Internet Explorer的最新版，以获得最佳体验。</p>
                     </div>
                 </div>
                 <!--温馨提示 end-->
