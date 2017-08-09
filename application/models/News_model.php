4c0acb169b5c3a2ca3b3f519d46f17a748a09b9c
huaiping <huaiping@live.cn>
2017-08-09 09:45:51 +0800

    news model improved

--- application/models/News_model.php
+++ application/models/News_model.php
@@ -35,43 +35,28 @@ class News_model extends CI_Model {
     }
 
     /*首页 - 视频点播*/
-    public function get_video($id = FALSE)
+    public function get_video()
     {
-        if ($id === FALSE)
-        {
-            $this->db->order_by('pubtime', 'DESC');
-            $this->db->limit(10);
-            $query = $this->db->get('swan_video');
-            return $query->result_array();
-        }
-        $query = $this->db->get_where('swan_video', array('id' => $id));
-        return $query->row_array();
+        $this->db->order_by('pubtime', 'DESC');
+        $this->db->limit(10);
+        $query = $this->db->get('swan_video');
+        return $query->result_array();
     }
 
     /*首页 - 最新资源*/
-    public function get_download($id = FALSE)
+    public function get_download()
     {
-        if ($id === FALSE)
-        {
-            $this->db->order_by('pubtime', 'DESC');
-            $this->db->limit(12);
-            $query = $this->db->get('swan_download');
-            return $query->result_array();
-        }
-        $query = $this->db->get_where('swan_download', array('id' => $id));
-        return $query->row_array();
+        $this->db->order_by('pubtime', 'DESC');
+        $this->db->limit(12);
+        $query = $this->db->get('swan_download');
+        return $query->result_array();
     }
 
     /*首页 - 友情链接*/
-    public function get_friendsites($id = FALSE)
+    public function get_friendsites()
     {
-        if ($id === FALSE)
-        {
-            $query = $this->db->get('swan_links');
-            return $query->result_array();
-        }
-        $query = $this->db->get_where('swan_links', array('id' => $id));
-        return $query->row_array();
+        $query = $this->db->get('swan_links');
+        return $query->result_array();
     }
 
     /*新闻中心 - 新闻列表*/
