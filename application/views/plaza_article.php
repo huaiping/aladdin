ef3aa3f41275e8ee7726f277eb2e44967037386e
huaiping <huaiping@live.cn>
2017-08-09 21:24:52 +0800

    plaza improved

--- application/views/plaza_article.php
+++ application/views/plaza_article.php
@@ -7,7 +7,7 @@
                 <li class="text-center"><a href="#"><img src="<?php echo base_url('avatar/avatar.php?char='.$this->session->userdata('name')); ?>" alt=""></a></li>
                 <li><a href="#">用户名：<?php echo $this->session->userdata('username'); ?></a></li>
                 <li><a href="#">用户组：<?php echo $this->session->userdata('groups'); ?></a></li>
-                <li><a href="#">注册时间：2015-6-10</a></li>
+                <li><a href="#">注册时间：<?php echo substr($this->session->userdata('pubtime'),0,10); ?></a></li>
                 <li><a href="#">当前IP：<?php echo $this->input->ip_address(); ?></a></li>
             </ul>
             <!--左侧用户中心 end-->
