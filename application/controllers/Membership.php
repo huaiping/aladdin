9e6e3cd57d0c17f49e1d4927d4826f2e4dce217a
huaiping <huaiping@live.cn>
2017-08-10 19:10:46 +0800

    levels improved

--- application/controllers/Membership.php
+++ application/controllers/Membership.php
@@ -81,20 +81,33 @@ class Membership extends CI_Controller {
         );
         $data = $this->security->xss_clean($data);
         $this->membership_model->register($data);
-        redirect('/membership/login');
+        redirect('membership');
     }
 
     public function plaza()
     {
-        $data['articles'] = $this->membership_model->get_articles();
-        $data['albums'] = $this->membership_model->get_albums();
-        $data['files'] = $this->membership_model->get_files();
-        $data['messages'] = $this->membership_model->get_messages();
-        $data['videos'] = $this->membership_model->get_videos();
-        $data['links'] = $this->membership_model->get_links();
-        $this->load->view('plaza_header');
-        $this->load->view('plaza', $data);
-        $this->load->view('footer');
+        $levels = $this->session->userdata('levels');
+        if ($levels == 1) {
+            $this->load->view('plaza_header');
+
+            $this->load->view('footer');
+        } elseif ($levels == 2) {
+            $data['articles'] = $this->membership_model->get_articles();
+            $data['albums'] = $this->membership_model->get_albums();
+            $data['files'] = $this->membership_model->get_files();
+            $data['messages'] = $this->membership_model->get_messages();
+            $data['videos'] = $this->membership_model->get_videos();
+            $data['links'] = $this->membership_model->get_links();
+            $this->load->view('plaza_header');
+            $this->load->view('plaza', $data);
+            $this->load->view('footer');
+        } elseif ($levels == 3) {
+            $this->load->view('plaza_header');
+            $this->load->view('plaza_message');
+            $this->load->view('footer');
+        } else {
+            redirect('membership');
+        }
     }
 
     public function article()
@@ -108,9 +121,7 @@ class Membership extends CI_Controller {
         }
         else
         {
-            $this->load->view('header');
-            $this->load->view('login');
-            $this->load->view('footer');
+            redirect('membership');
         }
     }
 
@@ -140,9 +151,7 @@ class Membership extends CI_Controller {
         }
         else
         {
-            $this->load->view('header');
-            $this->load->view('login');
-            $this->load->view('footer');
+            redirect('membership');
         }
     }
 
@@ -156,9 +165,7 @@ class Membership extends CI_Controller {
         }
         else
         {
-            $this->load->view('header');
-            $this->load->view('login');
-            $this->load->view('footer');
+            redirect('membership');
         }
     }
 
@@ -199,9 +206,16 @@ class Membership extends CI_Controller {
 
     public function message()
     {
-        $this->load->view('plaza_header');
-        $this->load->view('plaza_message');
-        $this->load->view('footer');
+        if ($this->is_login())
+        {
+            $this->load->view('plaza_header');
+            $this->load->view('plaza_message');
+            $this->load->view('footer');
+        }
+        else
+        {
+            redirect('membership');
+        }
     }
 
     public function messagePost($title = FALSE, $content = FALSE, $username = FALSE)
@@ -229,9 +243,7 @@ class Membership extends CI_Controller {
         }
         else
         {
-            $this->load->view('header');
-            $this->load->view('login');
-            $this->load->view('footer');
+            redirect('membership');
         }
     }
 
@@ -276,9 +288,7 @@ class Membership extends CI_Controller {
         }
         else
         {
-            $this->load->view('header');
-            $this->load->view('login');
-            $this->load->view('footer');
+            redirect('membership');
         }
     }
 
@@ -308,9 +318,7 @@ class Membership extends CI_Controller {
         }
         else
         {
-            $this->load->view('header');
-            $this->load->view('login');
-            $this->load->view('footer');
+            redirect('membership');
         }
     }
 
