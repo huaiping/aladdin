18632455d65da9abca6db77cb9ea526a61f1281d
huaiping <huaiping@live.cn>
2017-08-14 11:35:28 +0800

    exam improved

--- application/views/exam.php
+++ application/views/exam.php
@@ -12,39 +12,46 @@
     <div class="exam row">
         <div class="col-md-12">
             <div class="col-md-9">
-                <!--顶部题号 begin-->
-                <nav aria-label="Page navigation" style="margin-top:-20px; margin-left:-15px;">
-                    <ul class="pagination pagination-lg exam-pagination">
-                        <li class="disabled"><a href="#">题号</a></li>
-                        <li><a href="#" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>
-                        <li class="active"><a href="#">21 <span class="sr-only">(current)</span></a></li>
-                        <li><a href="#">22</a></li>
-                        <li><a href="#">23</a></li>
-                        <li><a href="#">24</a></li>
-                        <li><a href="#">25</a></li>
-                        <li><a href="#">26</a></li>
-                        <li><a href="#">27</a></li>
-                        <li><a href="#">28</a></li>
-                        <li><a href="#">29</a></li>
-                        <li><a href="#">30</a></li>
-                        <li><a href="#" aria-label="Next"><span aria-hidden="true">&raquo;</span></a></li>
-                    </ul>
-                </nav>
-                <!--顶部题号 begin-->
-
-                <!--左侧试题 begin-->
+                <!--左侧试题区 begin-->
                 <div class="widget-box no-border">
-                    <div class="widget-box_title bs-callout bs-callout-info" style="margin-left:-15px;"><h4>试题</h4></div>
-                        <ul class="widget-links list-unstyled">
-                            test test test<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+                    <div class="widget-box_title bs-callout bs-callout-info" style="margin-left:-15px;"><h4>试题<span class="pull-right glyphicon glyphicon-menu-hamburger" style="padding-right:10px;"></span></h4></div>
+                    <!--题号 begin-->
+                    <nav aria-label="Page navigation" style="margin-top:-20px; margin-left:-15px;">
+                        <ul class="pagination pagination-lg exam-pagination">
+                            <li class="disabled"><a href="#">题号</a></li>
+                            <li><a href="#" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>
+                            <li class="active"><a href="#">21 <span class="sr-only">(current)</span></a></li>
+                            <li><a href="#">22</a></li>
+                            <li><a href="#">23</a></li>
+                            <li><a href="#">24</a></li>
+                            <li><a href="#">25</a></li>
+                            <li><a href="#">26</a></li>
+                            <li><a href="#">27</a></li>
+                            <li><a href="#">28</a></li>
+                            <li><a href="#">29</a></li>
+                            <li><a href="#">30</a></li>
+                            <li><a href="#" aria-label="Next"><span aria-hidden="true">&raquo;</span></a></li>
                         </ul>
-                    </div>
-                <!--左侧试题 end-->
+                    </nav>
+                    <!--题号 begin-->
+
+                    <!--试题 begin-->
+                    <ul class="widget-links list-unstyled">
+                        test
+                    </ul>
+                    <!--试题 end-->
+                </div>
+                <!--左侧试题区 end-->
             </div>
 
             <div class="col-xs-12 col-md-3">
                 <!--右侧考生信息和倒计时 begin-->
-                00:40:00
+                <div class="widget-box no-border">
+                    <div class="widget-box_title bs-callout bs-callout-danger"><h4>考试信息<span class="pull-right glyphicon glyphicon-menu-hamburger" style="padding-right:10px;"></span></h4></div>
+                    <ul class="widget-links list-unstyled">
+                        00:40:00
+                    </ul>
+                </div>
                 <!--右侧考生信息和倒计时 end-->
             </div>
         </div>
