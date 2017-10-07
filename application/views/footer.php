457c19ad126e414577bb81e13e5b47e19153af4c
huaiping <huaiping@live.cn>
2017-10-07 11:56:26 +0800

    beian added

--- application/views/footer.php
+++ application/views/footer.php
@@ -4,9 +4,17 @@
     <div class="container bg-primary">
         <div class="row">
             <div class="col-xs-12 col-sm-10 col-md-10">
-                <div style="height:30px; line-height:30px; margin-top:10px;">Copyright &copy; 2015-2017 ♂古月Sir@阿拉丁CMS</div>
-                <div style="height:30px; line-height:30px;">地址：湖北省麻城市建设西路31号 ┋ 邮编：438300</div>
-                <div style="height:30px; line-height:30px; margin-bottom:10px;">鄂ICP备15002830号 Powered by 阿拉丁CMS</div>
+                <div style="line-height:30px; margin-top:10px;">Copyright &copy; 2015-2017 ♂古月Sir@阿拉丁CMS</div>
+                <div style="line-height:30px;">地址：湖北省麻城市建设西路31号 ┋ 邮编：438300</div>
+                <div style="line-height:30px; margin-bottom:10px;">
+                    <div class="pull-left" style="margin-right:20px;">
+                        <a href="//www.beian.gov.cn/portal/registerSystemInfo?recordcode=" style="color:#fff; text-decoration:none;"><img src="<?php echo base_url('webroot/image/beian.png'); ?>" alt=""><span>鄂公网安备xxxxxx号</span></a>
+                    </div>
+                    <div class="pull-left" style="margin-bottom:10px;">
+                        <a href="//www.miibeian.gov.cn" style="color:#fff; text-decoration:none;"><span>鄂ICP备15002830号</span></a>
+                        <span style="margin-left:15px;">Powered by 阿拉丁CMS</span>
+                    </div>
+                </div>
             </div>
             <div class="col-sm-2 col-md-2 text-right hidden-xs" style="margin-top:39px;">
                 <img src="<?php echo base_url('webroot/image/html5.png'); ?>" alt="HTML5">
