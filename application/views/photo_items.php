b49f7d21d02f3a70079da611d0ccffaeb8061dc8
huaiping <huaiping@live.cn>
2017-10-19 11:26:04 +0800

    updated

--- application/views/photo_items.php
+++ application/views/photo_items.php
@@ -15,7 +15,7 @@
                 <div class="widget-box_title bs-callout bs-callout-info">
                     <h4><?php echo $photos[0]['category'].'_'.$photos[0]['subcategory']; ?><span class="pull-right fa fa-bars" style="padding-right:10px;"></span></h4>
                 </div>
-                <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                <div id="carousel-generic" class="carousel slide" data-ride="carousel">
                     <!-- Wrapper for slides -->
                     <div class="carousel-inner" role="listbox">
                         <?php for($i=0; $i<count($photos); $i++): ?>
@@ -26,11 +26,11 @@
                         <?php endfor; ?>
                     </div>
                     <!-- Controls -->
-                    <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                    <a class="left carousel-control" href="#carousel-generic" role="button" data-slide="prev">
                         <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
                         <span class="sr-only">上一张</span>
                     </a>
-                    <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                    <a class="right carousel-control" href="#carousel-generic" role="button" data-slide="next">
                         <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
                         <span class="sr-only">下一张</span>
                     </a>
