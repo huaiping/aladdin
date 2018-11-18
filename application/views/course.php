f68fbe9909dd07f181412ef09b375521d3cc3161
huaiping <huaiping@live.cn>
2018-11-18 14:24:05 +0800

    updated

--- application/views/course.php
+++ application/views/course.php
@@ -1,19 +1,10 @@
 <section class="container">
     <?php
         $attributes = array('class' => 'form-horizontal', 'id' => 'course');
-        $hidden = array('name' => $this->session->userdata('name'));
+        $hidden = array('xh' => $this->session->userdata('username'), 'name' => $this->session->userdata('name'));
         echo form_open('course/post', $attributes, $hidden);
     ?>
     <!--<form method="post" id="courses" name="courses" action="#" class="form-horizontal">-->
-        <div class="panel panel-info"></div>
-        <div class="form-group">
-            <div class="text-center" style="margin-top:10px; margin-bottom:10px;">
-                <button type="submit" class="btn btn-success">提交</button>
-            </div>
-        </div>
-    </form>
-
-
         <div class="checkbox">
             <label><input name="course" type="checkbox" value="">物理</label>
         </div>
@@ -32,20 +23,22 @@
         <div class="checkbox">
             <label><input name="course" type="checkbox" value="">地理</label>
         </div>
+        <div class="form-group">
+            <div class="text-center" style="margin-top:10px; margin-bottom:10px;">
+                <button type="submit" class="btn btn-success">提交</button>
+            </div>
+        </div>
     </form>
-    <button type="submit" class="btn btn-success">提交</button>
-
-
 </section>
 
 <script>
 $(".btn").click(function(){
     if ($('input[name="course"]:checked').length < 3) {
         //$(this).attr("checked","");
-        alert("少于3个");
+        alert("选课科目少于3个");
     } else if ($('input[name="course"]:checked').length > 3) {
         //$(this).attr("checked","");
-        alert("最多能选3个");
+        alert("选课科目大于3个");
     }
 });
 </script>
