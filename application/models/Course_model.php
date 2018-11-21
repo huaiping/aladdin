1bdc4249aef7e067989fc0abeb1952ea523c30ea
huaiping <huaiping@live.cn>
2018-11-21 16:03:28 +0800

    updated

--- application/models/Course_model.php
+++ application/models/Course_model.php
@@ -6,17 +6,25 @@ class Course_model extends CI_Model {
         $this->load->database();
     }
 
-    /*选课报名*/
+    /*选课报名 - 数据提交*/
     public function coursePost($data)
     {
         $this->db->insert('swan_course', $data);
     }
 
-    /*选课状态*/
+    /*选课报名 - 选课状态*/
     public function get_status($xh = FALSE, $xm = FALSE)
     {
         $query = $this->db->get_where('swan_course', array('xh' => $xh, 'xm' => $xm));
         return $query->row_array();
+    }
+
+    /*选课报名 - 数据统计*/
+    public function get_statistics()
+    {
+        $this->db->group_by("courses");
+        $query = $this->db->get_where('swan_course'));
+        return $query->result_array();
     }
 
 }
