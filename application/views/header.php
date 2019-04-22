9d1aaf8b0c6dc9d839990a33a7ce03a4e177c12e
huaiping <huaiping@live.cn>
2019-04-22 10:40:37 +0800

    update bootstrap-select to v1.13.10

--- application/views/header.php
+++ application/views/header.php
@@ -9,7 +9,7 @@
 <title>阿拉丁CMS</title>
 <link rel="stylesheet" href="//cdn.bootcss.com/twitter-bootstrap/3.4.1/css/bootstrap.min.css">
 <link rel="stylesheet" href="//cdn.bootcss.com/font-awesome/5.8.1/css/all.min.css">
-<link rel="stylesheet" href="//cdn.bootcss.com/bootstrap-select/1.13.9/css/bootstrap-select.min.css">
+<link rel="stylesheet" href="//cdn.bootcss.com/bootstrap-select/1.13.10/css/bootstrap-select.min.css">
 <link rel="stylesheet" href="//cdn.bootcss.com/fancybox/3.5.7/jquery.fancybox.min.css">
 <link rel="stylesheet" href="//cdn.bootcss.com/video.js/7.5.4/video-js.min.css">
 <link rel="stylesheet" href="//cdn.bootcss.com/iCheck/1.0.2/skins/minimal/blue.css">
@@ -82,7 +82,7 @@ video::-webkit-media-controls-panel {width:calc(100% + 30px);}
 </style>
 <script src="//cdn.bootcss.com/jquery/3.4.0/jquery.min.js"></script>
 <script src="//cdn.bootcss.com/twitter-bootstrap/3.4.1/js/bootstrap.min.js"></script>
-<script src="//cdn.bootcss.com/bootstrap-select/1.13.9/js/bootstrap-select.min.js"></script>
+<script src="//cdn.bootcss.com/bootstrap-select/1.13.10/js/bootstrap-select.min.js"></script>
 <script src="//cdn.bootcss.com/jquery.qrcode/1.0/jquery.qrcode.min.js"></script>
 <script src="//cdn.bootcss.com/jquery.form/4.2.2/jquery.form.min.js"></script>
 <script src="//cdn.bootcss.com/highcharts/7.1.1/highcharts.js"></script>
